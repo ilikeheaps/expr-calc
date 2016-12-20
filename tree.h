02b47bc3e62aac901d02b01d99e85b3d24332c32
Wojciech Jasiński <ilikeheaps@gmail.com>
2016-12-20 14:03:52 +0100

    Added a comment

--- tree.h
+++ tree.h
@@ -5,6 +5,7 @@ Tree* new Tree(void* value);
 
 Tree* joinTrees(void* value, Tree** trees);
 
+//careful: fun must deallocate the second argument if it doesn't keep it
 void* TreeDFT(Tree* startNode, void* (*fun)(void*, void**));
 
 void* deleteTree(Tree* root, void* (*deleteValue)(void*));
