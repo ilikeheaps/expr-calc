8cdddd0e37ed6efbfd88539e0b1288850a04a8f5
Wojciech Jasiński <ilikeheaps@gmail.com>
2016-12-28 02:08:49 +0100

    Added associativity field

--- operator.h
+++ operator.h
@@ -6,12 +6,19 @@ typedef enum
     infix
 } notation_type;
 
+typedef enum
+{
+    left,
+    right
+} direction_type;
+
 typedef struct
 {
     int priority;
     int arity;
     double (*function)(double**); //takes a NULL-terminated array of arity values as argument
     notation_type notation;
+    direction_type assoc; //associativity
 } Operator;
 
 int applyOpToSt(Operator*, Stack*);
