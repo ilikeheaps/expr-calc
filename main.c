369cb17d4ea742e0f2e249c5a0258f697ebb67ba
Wojciech Jasiński <ilikeheaps@gmail.com>
2016-12-28 02:35:43 +0100

    Finished a sketch of the notation conversion algorithm

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ typedef struct
     void* value;
 } Token;
 
-Tree* eval(Token** expr, int tokenCount)
+Tree* makeOpTree(Token** expr)
 {
     Stack* operators = newStack();
     if(operators == NULL)
@@ -26,7 +26,7 @@ Tree* eval(Token** expr, int tokenCount)
         return;
     }
     
-    //values - stack of trees that represent a value
+    //values - stack of trees that represent a value to be calculated
     //  (the value can be obtained by traversing the tree)
     Stack* values = newStack();
     if(values = null)
@@ -36,34 +36,54 @@ Tree* eval(Token** expr, int tokenCount)
         return;
     }
     
-    for(int i=0; i<tokenCount; i++)
+    for(Token** currentToken = expr; currentToken != NULL; currentToken++)
     {
-        switch(expr[i]->type)
+        switch((*currentToken)->type)
         {
             //note: applying operators means applying them to the top elements of the values stack
             case operator:
-                Operator* currOp = expr[i] -> value;
-                /*TODO
-                 * 1) apply operators to values until the priority of the top element of the stack is equal to or lower than the priority of the current element (or only lower depending on the associativity)
-                 * 2) push the new operator onto the stack
-                 */
+                Operator* currOp = (*currentToken) -> value;
+                if(currOp -> assoc == left)
+                {
+                    //apply operators to values until the priority of the top element of the stack is lower than the priority of the current element
+                    while(emptySt(operators) || topSt(operators) -> priority < currOp -> priority)
+                    {
+                        Operator* opTop = (Operator*) popSt(operators);
+                        //TODO: error handling
+                        applyOpToSt(opTop, values);
+                        //TODO: error handling
+                    }
+                }
+                else //right-associative
+                {
+                    //apply operators to values until the priority of the top element of the stack is lower than the priority of the current element
+                    while(emptySt(operators) || topSt(operators) -> priority <= currOp -> priority)
+                    {
+                        Operator* opTop = (Operator*) popSt(operators);
+                        //TODO: error handling
+                        applyOpToSt(opTop, values);
+                        //TODO: error handling
+                    }
+                }
+                //push the new operator onto the stack
+                pushSt(operators, currOp);
                 break;
             case value:
-                Tree* valueTree = newTree(expr[i] -> value);
+                Tree* valueTree = newTree((*currentToken) -> value);
                 if(valueTree == NULL)
                 {
                     printf("eval line __LINE__: couldn't allocate tree node\n");
                     //TODO: deallocate stacks
                 }
-                Tree* newVal = newTree(expr[i]) -> value);
+                Tree* newVal = newTree((*currentToken)) -> value);
                 if(newVal == NULL)
                 {
-                    //TODO: error
+                    //TODO: error handling
                     return;
                 }
                 if(pushSt(values, newVal))
                 {
-                    //TODO: error
+                    //TODO: error handling
                     return;
                 }
                 break;
@@ -72,11 +92,11 @@ Tree* eval(Token** expr, int tokenCount)
                 break;
             case endBracket:
                 //TODO: handle errors: popSt, applyOp
-                Operator* opTop = popSt(operators)
+                Operator* opTop = (Operator*) popSt(operators)
                 while(opTop -> type != openBracket)
                 {
-                    applyOp(values, opTop)
-                    opTop = popSt(operators);
+                    applyOpToSt(opTop, values)
+                    opTop = (Operator*) popSt(operators);
                 }
                 //removes opening bracket
                 (void) popSt(operators);
@@ -86,10 +106,20 @@ Tree* eval(Token** expr, int tokenCount)
         
     }
     
-    //TODO: apply operators to values until there is only one value left
-    // if there are any operators left, return some error
     
-    return topSt(values);
+    //apply operators to values until there is only one value left
+    //TODO: if there are any operators left, return some error
+    while(sizeSt(values) > 1)
+    {
+        Operator* opTop = topSt(operators);
+        //TODO: error handling
+        applyOpToSt(opTop, values);
+    }
+    Tree* ans = popSt(values);
+        
+    //TODO: deallocate stuff (stacks and perhaps something more)
+    
+    return ans;
 }
 
 int main(int argc, char* args[])
